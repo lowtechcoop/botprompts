d1e3edb56a7c95282565e6b75ce823872da836c2
ltnyet <130899831+ltnyet@users.noreply.github.com>
2023-04-19 12:46:18 +1000

    Updated launch.sh to include --allow-forwarded-ips for uvicorn

--- config/docker/backend/launch.sh
+++ config/docker/backend/launch.sh
@@ -4,4 +4,4 @@ echo "Applying alembic migrations"
 alembic upgrade head || exit 1
 
 echo "Starting API"
-uvicorn app.main:app --host "0.0.0.0" --port 8000 --proxy-headers --no-server-header
+uvicorn app.main:app --host "0.0.0.0" --port 8000 --proxy-headers --no-server-header --forwarded-allow-ips='*'
